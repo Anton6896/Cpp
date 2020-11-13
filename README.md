37ed90953d516085f764b214776dd9516a3eeb2f
ant_pc <anton-r@live.com>
2020-11-13 21:30:53 +0500

    hgh

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Cpp
-C++ learn . Udemy with Jason Fedin ( by the way amazing teacher !!! )
+C++ learn . Udemy course 
--- first/hello.cpp
+++ first/hello.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
-int my_first_main() {
-    // comment
-    std::cout << "hello world !" << std::endl;
+int user_asq()
+{
+    std::cout << "enter your name : " << std::endl;
+
+    return 0;
+}
+
+int my_first_main()
+{
+    std::cout << "firs things first :" << std::endl;
+    user_asq();
     return 0;
 }
