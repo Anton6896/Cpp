ad6a7c1d668b3c1e6c26cce99ddbf806ffbd73b4
Anton.R <Anton-r@ive.com>
2020-11-13 20:36:17 +0500

    ss

---
+++ first/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int my_first_main() {
+    std::cout << "hello world !" << std::endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+
+#include "first/hello.cpp"
+
+int main() {
+    my_first_main();
+
+    return 0;
+}
