0bc2505f010d09586a3974d5976252184cc9156c
Ant_win <anton-r@live.com>
2020-11-14 09:22:05 +0500

    dsds

--- first/hello.cpp
+++ first/hello.cpp
@@ -3,18 +3,19 @@
 using namespace std;
 
 int user_asq() {
+    // cpp will attach the int , double and string to var by it self 
 
-    int num;
-    cout << "enter your favorite number  :";
-    cin >> num;
-
+    int num1, num2;
+    cout << "enter your favorite numbers: " << endl;
+    cin >> num1 >> num2; // chain insertion
+    cout << "num 1: " << num1 << ", num 2: " << num2 << endl; // chain output
 
 
     return 0;
 }
 
 int my_first_main() {
-    std::cout << "firs things first :" << std::endl;
+    cout << "firs things first :\n";
     user_asq();
     return 0;
 }
