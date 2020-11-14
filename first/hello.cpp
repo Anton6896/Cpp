3c8e5878e5030a205ce2bf04e8b7c5589dfb45f0
Ant_win <anton-r@live.com>
2020-11-14 10:08:30 +0500

    constant var

--- first/hello.cpp
+++ first/hello.cpp
@@ -14,8 +14,45 @@ int user_asq() {
     return 0;
 }
 
+void var_main() {
+
+//    cout << "enter height nad width :" << endl;
+//    double height, width;
+//    cin >> height >> width;
+//    cout << "room area " << (height * width) << endl;
+
+    int mint = 1;
+    unsigned int muint = 1; // >0
+    double mdouble = 1.1;
+    char mchar = 'c';
+    string mstring = "mstring";
+    bool mbool = true;
+    long long mlong = 7'435'342;
+    long double mlodo = 2.7e120;
+
+    // regular sizing
+    cout << "\nsizing of variables :\n";
+    cout << "int: " << sizeof(mint) << endl;
+    cout << "un signet int: " << sizeof(muint) << endl;
+    cout << "char: " << sizeof(mchar) << endl;
+    cout << "string: " << size(mstring) << endl; // return size of container
+    cout << "long long (big int): " << sizeof(mlong) << endl;
+    cout << "long double (super big): " << sizeof(mlodo) << endl;
+    cout << "bool: " << sizeof(mbool) << "\n" << endl;
+
+
+    unsigned int n1 = 1; // >0
+    unsigned int n2 = 2; // >0
+    int n3 = INT_MAX;
+    cout << (n1 - n2) << endl; // return some positive num
+    cout << "max int : " << n3 << endl;
+
+
+}
+
 int my_first_main() {
     cout << "firs things first :\n";
-    user_asq();
+//    user_asq();
+    var_main();
     return 0;
 }
