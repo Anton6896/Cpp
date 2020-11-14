1936412a570c3a9438850d56e1979bc30080b56f
Ant_win <anton-r@live.com>
2020-11-14 11:13:07 +0500

    constant var

---
+++ arr_and_vectors/arr_and_vec.cpp
@@ -0,0 +1,14 @@
+//
+// Created by User on 11/14/2020.
+//
+
+#include <iostream>
+
+
+int av_main(){
+
+
+
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
 #include "first/hello.cpp"
+#include "arr_and_vectors/arr_and_vec.cpp"
 
 int main() {
-    my_first_main();
+    cout << "main : \n";
+//    my_first_main();
+    av_main();
 
+    cout << "\n";
     return 0;
 }
 
