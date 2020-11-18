aa4a854cf4a4e6cb2add9ee723ed338bc0d1bc6c
Mac <Anton-r@live.com>
2020-11-18 09:41:54 +0500

    ss

--- pointer_my/pointer_main.cpp
+++ pointer_my/pointer_main.cpp
@@ -14,9 +14,24 @@ void point_main() {
     cout << "size of  : " << sizeof(*pnum) << endl;
     cout << "address : " << &pnum << endl;
 
-    int num = 40;
-    pnum = &num;
-    cout << "data after pointer init : " << *pnum << endl;
+    cout << "\n **** allocating memory from heap : " << "\n";
+    int *int_ptr{nullptr};
+    int_ptr = new int; // allocating memory frm heap
+    cout << int_ptr << "\n"; // got address on heap
+    cout << *int_ptr << "\n";  // trash dta
+    *int_ptr = 10; // init data at this location
+    cout << *int_ptr << "\n";
+    delete int_ptr; // free memory
+
+    // arr pointers
+    double *parr_1{nullptr};
+    double arr[]{12.3, 32.5};
+    parr_1 = arr;
+    cout << (&arr) << endl;
+    cout << (parr_1) << endl;
+
+
+    delete parr_1; // deallocate memory
 
 
 }
