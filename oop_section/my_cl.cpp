8305535aef8d41eb686bc56bee4a94e455737d36
Ant_win <anton-r@live.com>
2020-11-19 15:02:52 +0500

    oop

--- oop_section/my_cl.cpp
+++ oop_section/my_cl.cpp
@@ -4,15 +4,58 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-class Person {
-    string name;
-    int age;
+int counter = 1;
 
-    void speak() {
+class MyAccount {
+//    MyPerson *person = new MyPerson; // connect to other class (insane)
+    int id = counter++;
+    double balance = 0.0;
+
+public:
+    bool deposit(double sum) {
+        balance += sum;
+        return true;
+    }
+
+    int get_id() {
+        return id;
+    }
+
+    double get_balance() {
+        return balance;
+    };
+
+    bool withdraw(double sum) {
+        if (sum < balance) {
+            balance -= sum;
+        } else {
+            cout << "no money" << endl;
+            return false;
+        }
+        return true;
+    };
+};
+
+
+class MyPerson {
+public:
+    string name = "name";
+    int age = 10;
+    MyAccount *acc_prt = new MyAccount;
+
+
+    void speak() const {
         cout << " my name is " << name << endl;
         cout << " my age is " << age << endl;
+        cout << " my balance is " << acc_prt->get_balance() << endl;
+        cout << " my id is " << acc_prt->get_id() << endl;
     }
+
+
 };
+
+
--- oop_section/oop_main.cpp
+++ oop_section/oop_main.cpp
@@ -9,14 +9,52 @@ using namespace std;
 
 
 /// declare _____________________________________________________
-
+// my_cl.cpp
 
 
 /// main ________________________________________________________
 void oop_main() {
     cout << "oop main function start : " << endl;
 
+    MyPerson per1, per2;
+    vector<MyPerson> person_vec{per1};  // with objects
+    person_vec.emplace_back(per2);
 
+    // access if public
+    person_vec.at(0).name = "bob";
+    person_vec.at(0).age = 33;
+    person_vec.at(0).speak();
+    cout << endl;
+
+    auto *per1_ptr = new MyPerson;
+    per1_ptr->name = "name in poiner";
+    per1_ptr->age = 23;
+    per1_ptr->speak();
+    delete per1_ptr; // remove if no needed
+
+    // nested class access
+    cout << endl;
+    auto *per2_ptr = new MyPerson;
+    per2_ptr->name = "per2 name";
+    per2_ptr->age = 34;
+    per2_ptr->acc_prt->deposit(1000);
+    per2_ptr->acc_prt->deposit(200);
+    per2_ptr->acc_prt->withdraw(100);
+    per2_ptr->speak();
+
+    cout << endl;
+    auto *per3_ptr = new MyPerson;
+    per3_ptr->name = "per3 name";
+    per3_ptr->age = 20;
+    per3_ptr->acc_prt->deposit(600);
+    per3_ptr->acc_prt->deposit(200);
+    per3_ptr->acc_prt->withdraw(100);
+    per3_ptr->speak();
+
+    delete per2_ptr;
+    delete per3_ptr;
+
+    cout << "\nok ..";
 }
 
 
