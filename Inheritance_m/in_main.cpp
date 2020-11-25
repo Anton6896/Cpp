d39054b6aef7a61b0265dc966c4a31b006b93121
Ant_win <anton-r@live.com>
2020-11-25 10:48:34 +0500

    classes

--- Inheritance_m/in_main.cpp
+++ Inheritance_m/in_main.cpp
@@ -5,15 +5,25 @@
 
 using namespace std;
 
-int in_main(){
-    Account *acc1, acc2;
-    acc1 = new Account();
-    acc1->deposit(2000.34);
-    acc1->withdraw(34.6);
+int in_main() {
+    {
+//        Account *acc1, acc2;
+//        acc1 = new Account();
+//        acc1->deposit(2000.34);
+//        acc1->withdraw(34.6);
+//
+//
+//        Save_Account sacc_1;
+//        sacc_1.add_to_amount(40);
+//        sacc_1.deposit(320);
+//        delete acc1;
+    }
 
-    delete acc1;
 
-
+    {
+        A a1, a2(4);
+        B b1, b2(4);
+    }
 
     return 0;
 }
